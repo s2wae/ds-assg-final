0a95e014fbdf64587ea21914eb40bf03385908d0
s2wae <iantxh@gmail.com>
2024-12-28 15:18:28 +0800

    Added User and Menu classes

--- ds-assg-final/Main.java
+++ ds-assg-final/Main.java
@@ -1,5 +1,20 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException; 
+import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main{
+    
+
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Menu menu = new Menu();
+        
+        menu.chooseStartOption();
+
     }
+    
+
 }
---
+++ ds-assg-final/User.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class User {
+
+
+    public String username, password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean checkValidUser(String username, HashMap<String, String> user_db){
+        if(user_db.containsKey(username)){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkUserLogin(String username, String password, HashMap<String, String> user_db){
+
+        if(!user_db.containsKey(username) || user_db.get(username) == null){
+            return false;
+        }
+
+        if((!password.equals(user_db.get(username)))){
+            return false;
+        }
+
+        return true;
+    }
+
+    public HashMap<String, String> addUser(String username, String password, HashMap<String, String> user_db){
+        HashMap<String, String> temp_map = user_db;
+        temp_map.put(username, password);
+        return temp_map;
+    }
+
+}
